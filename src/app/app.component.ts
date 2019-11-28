558c9536384b53945ffdf250dbca9ba32200c78b
Surahutomo Pradana <surahutomo.pradana@btpn.com>
2019-11-28 15:59:22 +0700

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import 'node_modules/popper.js/dist/popper.min';
+import 'node_modules/bootstrap/dist/js/bootstrap';
+import 'node_modules/jquery';
+
+import '../assets/js/poseDetector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'tensor-test1';
+  render = () => {
+    console.log('rendering');
+  }
 }
